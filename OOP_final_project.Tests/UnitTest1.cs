3d4c03f00adf68bbb8fb389daef9058016479075
Raditya Wardhana <radityaparawar@gmail.com>
2025-12-14 20:26:25 +0700

    push

---
+++ OOP_final_project.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using OOP_final_project;
+using System;
+using System.Numerics;
+using Xunit;
+
+namespace OOP_final_project.Tests
+{
+    public class GlobalUserAndDerivedTests
+    {
+        [Fact]
+        public void New_User_Should_Have_NonEmpty_Id()
+        {
+            var doctor = new Doctor();
+            Assert.NotEqual(Guid.Empty, doctor.Id);
+        }
+    }
+}
