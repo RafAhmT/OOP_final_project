9899d0066d3a6563d7f530c3418f29d40f1a2b3f
Raditya Wardhana <radityaparawar@gmail.com>
2025-12-16 12:17:26 +0700

    Add Admin unit tests (Id, Name, Department, AccessLevel)

--- OOP_final_project_tests/UserTests.cs
+++ OOP_final_project_tests/UserTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 using OOP_final_project;
 
@@ -19,6 +20,35 @@ namespace OOP_final_project_tests
 
             // Assert
             Assert.Equal(name, admin.Name);
+        }
+
+        [Fact]
+        public void NewAdmin_ShouldGenerateNonEmptyId()
+        {
+            // Act
+            var admin = new Admin();
+
+            // Assert
+            Assert.NotEqual(Guid.Empty, admin.Id);
+        }
+
+        [Fact]
+        public void CreateAdmin_ShouldSetDepartmentAndAccessLevelCorrectly()
+        {
+            // Arrange
+            var department = "IT";
+            var accessLevel = 5;
+
+            // Act
+            var admin = new Admin
+            {
+                Department = department,
+                AccessLevel = accessLevel
+            };
+
+            // Assert
+            Assert.Equal(department, admin.Department);
+            Assert.Equal(accessLevel, admin.AccessLevel);
         }
     }
 }
